d8d738d175d9be82b7b5e0e414163375320de525
DenimCity <altidorj1@gmail.com>
2019-12-20 13:12:04 -0500

    chore: can now select the different categories

--- lib/widgets/category_selector.dart
+++ lib/widgets/category_selector.dart
@@ -6,7 +6,7 @@ class CategorySelector extends StatefulWidget {
 }
 
 class _CategorySelectorState extends State<CategorySelector> {
-  final selectedINdex = 0;
+  var selectedIndex = 0;
   final List<String> categories = ["Message", "Online", "Groups", "Requests"];
   @override
   Widget build(BuildContext context) {
@@ -17,15 +17,23 @@ class _CategorySelectorState extends State<CategorySelector> {
         scrollDirection: Axis.horizontal,
         itemCount: categories.length,
         itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-            child: Text(
-              categories[index],
-              style: TextStyle(
-                  color: index == selectedINdex ? Colors.white : Colors.white60,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.2),
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+              child: Text(
+                categories[index],
+                style: TextStyle(
+                    color:
+                        index == selectedIndex ? Colors.white : Colors.white60,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2),
+              ),
             ),
           );
         },
