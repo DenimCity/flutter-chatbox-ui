14bdf4e7a15139558b76a4120273b3c1d5ec5024
DenimCity <altidorj1@gmail.com>
2019-12-20 13:45:46 -0500

    add minute 18:19

--- lib/widgets/favorite_contact.dart
+++ lib/widgets/favorite_contact.dart
@@ -3,13 +3,26 @@ import 'package:flutter/material.dart';
 class FavoirtContacts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: <Widget>[
-        Text("Favorite Contacts"),
-        IconButton(
-          icon: Icon(Icons.more_horiz),
-          onPressed: () {},
-        )
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text("Favorite Contacts",
+                  style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0)),
+              IconButton(
+                icon: Icon(Icons.more_horiz),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
       ],
     );
   }
